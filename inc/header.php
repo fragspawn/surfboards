d5f73a2be528ac778a9b290e82cc4e0ff8010d88
jp <jp@blood.localdomain>
2020-10-25 17:12:29 +1000

    gtm test

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WF2FKHTRPE"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WF2FKHTRPE');
+        </script>
+
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MLKZVJD');
+        </script>
+
         <meta charset="utf-8" />
         <title>Matt's Surfboards</title>
         <!-- link to favicon -->
